148580941fba17b73d4fedb4ea7406200ad63d8c
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-20 17:49:42 -0300

    chore: comment out authentication and role logic for development

--- src/pages/__layouts/app.tsx
+++ src/pages/__layouts/app.tsx
@@ -6,24 +6,24 @@ import Cookies from 'js-cookie'
 import { useDecode } from '@/auth'
 
 export function AppLayout() {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
-  useEffect(() => {
-    const token = Cookies.get('token')
+  // useEffect(() => {
+  //   const token = Cookies.get('token')
 
-    if (!token) {
-      navigate('/sign-in', {
-        replace: true,
-      })
-    }
-  }, [navigate])
+  //   if (!token) {
+  //     navigate('/sign-in', {
+  //       replace: true,
+  //     })
+  //   }
+  // }, [navigate])
 
-  const decoded = useDecode()
+  // const decoded = useDecode()
   return (
     <div className="h-full min-h-screen">
       <Header isLogged />
       <div className="relative flex h-[48.5rem] w-full bg-slate-100 md:h-[55rem]">
-        <Menu role={decoded?.user.role} />
+        <Menu role={'dev'} />
         <div className="w-full overflow-y-auto">
           <Outlet />
         </div>
