fe64de7abcf803618f50656fc245f5620db69d05
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-21 17:36:21 -0300

    feat: design improvements and adjustments

--- src/pages/update-students.tsx
+++ src/pages/update-students.tsx
@@ -146,7 +146,7 @@ export function UpdateStudent() {
 
             <button
               type="submit"
-              className="ml-auto block rounded bg-blue-800 px-3 py-2 text-white hover:bg-blue-700"
+              className="ml-auto block rounded bg-pmpa-blue-500 px-3 py-2 text-white hover:bg-pmpa-blue-700"
             >
               Atualizar
             </button>
