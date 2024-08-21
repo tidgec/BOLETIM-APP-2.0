1e76029db914f00da055ea7de65e94909fe29570
igorabreu29 <abreusense@gmail.com>
2024-08-21 17:14:35 -0300

    feat: get course poles

---
+++ src/http/get-course-poles.ts
@@ -0,0 +1,33 @@
+import { api } from '@/lib/axios'
+import Cookies from 'js-cookie'
+
+export interface GetCoursePolesRequest {
+  courseId: string
+}
+
+export interface GetCoursePolesResponse {
+  poles: {
+    id: string
+    name: string
+  }[]
+}
+
+export async function getCoursePoles({
+  courseId,
+}: GetCoursePolesRequest): Promise<GetCoursePolesResponse> {
+  const token = Cookies.get('token')
+
+  const response = await api.get<GetCoursePolesResponse>(
+    `/courses/${courseId}/poles`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      // params
+    },
+  )
+
+  return {
+    poles: response.data.poles,
+  }
+}
