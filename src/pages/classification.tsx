8fe53668f1b861acca922a770163621cb926d061
igorabreu29 <abreusense@gmail.com>
2024-08-21 13:30:30 -0300

    style: eslint code

--- src/pages/classification.tsx
+++ src/pages/classification.tsx
@@ -1,6 +1,6 @@
 import OverallRating from '@/assets/overall-rating-icon.png'
 import ClassificationByPole from '@/assets/classification-by-pole-icon.png'
-import Second from '@/assets/second-icon.png'
+// import Second from '@/assets/second-icon.png'
 import AverageNotes from '@/assets/average-notes-icon.png'
 import AverageBehavior from '@/assets/average-behavior-icon.png'
 import { Link } from 'react-router-dom'
@@ -11,7 +11,7 @@ export function Classification() {
       <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
         Classificação
       </h2>
- 
+
       <div className="mx-auto my-6 grid w-full max-w-7xl grid-cols-1 justify-items-center gap-4 py-2 md:grid-cols-2 md:justify-items-start lg:grid-cols-3">
         <Link
           to={'/classification/courses-search-general-classification'}
@@ -33,7 +33,9 @@ export function Classification() {
             className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
             alt=""
           />
-          <span className="font-medium text-white">Classificação por polos</span>
+          <span className="font-medium text-white">
+            Classificação por polos
+          </span>
         </Link>
         <Link
           to={'#'}
@@ -55,9 +57,11 @@ export function Classification() {
             className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
             alt=""
           />
-          <span className="font-medium text-white">Média de comportamento - polos</span>
+          <span className="font-medium text-white">
+            Média de comportamento - polos
+          </span>
         </Link>
-        <Link
+        {/* <Link
           to={'#'}
           className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
         >
@@ -67,7 +71,7 @@ export function Classification() {
             alt=""
           />
           <span className="font-medium text-white">Dados - Segunda época</span>
-        </Link>
+        </Link> */}
       </div>
     </section>
   )
