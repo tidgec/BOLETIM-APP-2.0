56c54f873aae19cc1d975736476a3be67f481862
igorabreu29 <abreusense@gmail.com>
2024-08-21 13:24:27 -0300

    feat: add interceptor to redirect user

--- src/pages/__layouts/app.tsx
+++ src/pages/__layouts/app.tsx
@@ -4,26 +4,48 @@ import { Menu } from '@/components/menu'
 import { useEffect } from 'react'
 import Cookies from 'js-cookie'
 import { useDecode } from '@/auth'
+import { api } from '@/lib/axios'
+import { isAxiosError } from 'axios'
 
 export function AppLayout() {
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   const token = Cookies.get('token')
+  useEffect(() => {
+    const token = Cookies.get('token')
 
-  //   if (!token) {
-  //     navigate('/sign-in', {
-  //       replace: true,
-  //     })
-  //   }
-  // }, [navigate])
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.message
 
-  // const decoded = useDecode()
+          if (status === 401 && code === 'Unathourized.') {
+            navigate('/sign-in', { replace: true })
+          } else {
+            throw error
+          }
+        }
+      },
+    )
+
+    if (!token) {
+      navigate('/sign-in', {
+        replace: true,
+      })
+    }
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
+  const decoded = useDecode()
   return (
     <div className="h-full min-h-screen">
       <Header isLogged />
       <div className="relative flex h-[48.5rem] w-full bg-slate-100 md:h-[55rem]">
-        <Menu role={'dev'} />
+        <Menu role={decoded?.user.role} />
         <div className="w-full overflow-y-auto">
           <Outlet />
         </div>
