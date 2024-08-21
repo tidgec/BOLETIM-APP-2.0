bf3d227eb91fe8c7f94d30432b34f1ed107c49a2
igorabreu29 <abreusense@gmail.com>
2024-08-21 13:28:47 -0300

    style: eslint code

--- src/pages/search-notes-to-update.tsx
+++ src/pages/search-notes-to-update.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 export function SearchNotesToUpdate() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('')
   const [disciplinas] = useState([
     'Armamento Policial',
     'Direito Penal',
@@ -38,42 +38,43 @@ export function SearchNotesToUpdate() {
     'Defesa Pessoal',
     'Atividade Física',
     'Primeiros Socorros',
-  ]);
+  ])
 
   const filteredDisciplinas = searchTerm
     ? disciplinas.filter((disciplina) =>
-        disciplina.toLowerCase().includes(searchTerm.toLowerCase())
+        disciplina.toLowerCase().includes(searchTerm.toLowerCase()),
       )
-    : disciplinas;
+    : disciplinas
 
   return (
-    <div className="w-full container mx-auto p-4">
-     <section className='mx-auto w-full max-w[90rem]'>
-     <h2 className="w-full border-b-2 py-4 border-black text-xl font-semibold">
-        Disciplinas</h2>
-      <div className="flex items-center py-4 mb-4">
-        <input
-          type="text"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="Pesquise pela disciplina"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
+    <div className="container mx-auto w-full p-4">
+      <section className="max-w[90rem] mx-auto w-full">
+        <h2 className="w-full border-b-2 border-black py-4 text-xl font-semibold">
+          Disciplinas
+        </h2>
+        <div className="mb-4 flex items-center py-4">
+          <input
+            type="text"
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            placeholder="Pesquise pela disciplina"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {filteredDisciplinas.map((disciplina, index) => (
-          <div
-            key={index}
-            className="bg-pmpa-blue-500 hover:bg-pmpa-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus-shadow-outline"
-          >
-            <a href={`/disciplinas/${disciplina.toLowerCase()}`}>
-              {disciplina}
-            </a>
-          </div>
-        ))}
-      </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {filteredDisciplinas.map((disciplina, index) => (
+            <div
+              key={index}
+              className="focus-shadow-outline rounded bg-pmpa-blue-500 px-4 py-2 font-bold text-white hover:bg-pmpa-blue-700 focus:outline-none"
+            >
+              <a href={`/disciplinas/${disciplina.toLowerCase()}`}>
+                {disciplina}
+              </a>
+            </div>
+          ))}
+        </div>
       </section>
     </div>
-  );
+  )
 }
