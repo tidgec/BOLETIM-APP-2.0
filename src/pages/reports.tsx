ad70aa1da9ed7ff5b9dbc900237a73149ddcfb2d
igorabreu29 <abreusense@gmail.com>
2024-08-26 20:29:55 -0300

    style: eslint code format

--- src/pages/reports.tsx
+++ src/pages/reports.tsx
@@ -1,71 +1,66 @@
-import { useGetReports } from "@/hooks/use-get-reports";
+import { useGetReports } from '@/hooks/use-get-reports'
 
 export function Reports() {
   const { reports, isLoading } = useGetReports()
 
-return ( 
-  <div className="w-full py-6">
-  <section className="mx-auto w-full max-w-[90rem]">
-    <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
-      Relatórios
-    </h2>
+  return (
+    <div className="w-full py-6">
+      <section className="mx-auto w-full max-w-[90rem]">
+        <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+          Relatórios
+        </h2>
 
-    <div className="group relative mx-auto my-8 h-[46rem] max-h-screen  max-w-6xl overflow-auto rounded bg-white shadow-md p-4">
-      <div className="flex justify-between items-center mb-8">
-      <div className="flex w-full max-w-3xl items-center gap-4 rounded-full bg-white px-2 py-2">
-        <input
-          type="text"
-          placeholder="Busque pelo nome ..."
-          className="flex-1 roundend-lg border border-gray-500 bg-transparent wpx-4 rounded-lg px-4 py-2 focus:outline-none focus:border-pmpa-blue-500 "
-        />
-        <button className="hidden"></button>
-      </div>
-      <div className='flex items-center space-x-4'>
-        <label htmlFor="filter" className="block text-black font-bold ">
-        Filtrar Por:
-        </label>
-        <select
-          id="filter"
-          className=" flex-1 shadow  roundend-lg border border-gray-500 w-full rounded-lg py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-          <option value="Aluno">Aluno</option>
-          <option value="Gestor">Gestor</option>
-        </select>
-      </div>
-      </div>
-
-      <div className="text-center">
-        <button className="bg-pmpa-blue-700 text-white font-bold py-2 px-10 rounded ">
-          TODOS OS RELATÓRIOS
-        </button>
-
-        <div className='py-2 flex-1 items-center'>
-        <select
-          id="filter_reports"
-          className="text-center shadow roundend-lg border border-gray-500 rounded-lg py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ">
-          <option value="Todos">TODOS</option>
-          <option value="Adições">ADIÇÕES</option>
-          <option value="Remoções">REMOÇÕES</option>
-          <option value="Alterações">ALTERAÇÕES</option>
-          <option value="Login">LOGIN</option>
-        </select>
-      </div>
-      </div>
-
-      {isLoading && (
-        <p>Loading...</p>
-      )}
-
-      {!isLoading && (
-        reports?.map(report => {
-          return (
-            <div key={report.id}>
-              {report.content }
+        <div className="group relative mx-auto my-8 h-[46rem] max-h-screen  max-w-6xl overflow-auto rounded bg-white p-4 shadow-md">
+          <div className="mb-8 flex items-center justify-between">
+            <div className="flex w-full max-w-3xl items-center gap-4 rounded-full bg-white px-2 py-2">
+              <input
+                type="text"
+                placeholder="Busque pelo nome ..."
+                className="roundend-lg wpx-4 flex-1 rounded-lg border border-gray-500 bg-transparent px-4 py-2 focus:border-pmpa-blue-500 focus:outline-none "
+              />
+              <button className="hidden"></button>
             </div>
-          )
-        })
-      )}
+            <div className="flex items-center space-x-4">
+              <label htmlFor="filter" className="block font-bold text-black ">
+                Filtrar Por:
+              </label>
+              <select
+                id="filter"
+                className=" roundend-lg focus:shadow-outline  w-full flex-1 rounded-lg border border-gray-500 px-5 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+              >
+                <option value="Aluno">Aluno</option>
+                <option value="Gestor">Gestor</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="text-center">
+            <button className="rounded bg-pmpa-blue-700 px-10 py-2 font-bold text-white ">
+              TODOS OS RELATÓRIOS
+            </button>
+
+            <div className="flex-1 items-center py-2">
+              <select
+                id="filter_reports"
+                className="roundend-lg focus:shadow-outline rounded-lg border border-gray-500 px-4 py-2 text-center leading-tight text-gray-700 shadow focus:outline-none "
+              >
+                <option value="Todos">TODOS</option>
+                <option value="Adições">ADIÇÕES</option>
+                <option value="Remoções">REMOÇÕES</option>
+                <option value="Alterações">ALTERAÇÕES</option>
+                <option value="Login">LOGIN</option>
+              </select>
+            </div>
+          </div>
+
+          {isLoading && <p>Loading...</p>}
+
+          {!isLoading &&
+            reports?.map((report) => {
+              return <div key={report.id}>{report.content}</div>
+            })}
+        </div>
+      </section>
     </div>
-  </section>    
-  </div>
-);
+  )
 }
