bfb588d061f93cc20e18d31a1dffd785e7e3637e
igorabreu29 <abreusense@gmail.com>
2024-08-26 20:29:30 -0300

    feat: add courseId params

--- src/components/discipline.tsx
+++ src/components/discipline.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom'
 
 interface DisciplineProps {
+  courseId: string
   disciplineId: string
   name: string
 }
 
-export function Discipline({ disciplineId, name }: DisciplineProps) {
-  const currentUrl = window.location.href
+export function Discipline({ courseId, disciplineId, name }: DisciplineProps) {
+  const currentUrl = window.location.href.replace(`?courseId=${courseId}`, '')
 
   return (
     <Link
       key={disciplineId}
-      to={`${currentUrl}/disciplines/${disciplineId}`}
+      to={`${currentUrl}/disciplines/${disciplineId}?courseId=${courseId}`}
       className="focus-shadow-outline rounded bg-pmpa-blue-500 px-4 py-2 font-bold text-white hover:bg-pmpa-blue-700 focus:outline-none"
     >
       {name}
