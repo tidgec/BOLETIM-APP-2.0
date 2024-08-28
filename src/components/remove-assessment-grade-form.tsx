5004db413ce79dd25f556b279c31970375d25b47
igorabreu29 <abreusense@gmail.com>
2024-08-28 12:52:22 -0300

    feat: add remove and update form

---
+++ src/components/remove-assessment-grade-form.tsx
@@ -0,0 +1,158 @@
+import { useRemoveAssessmentGrade } from '@/hooks/use-remove-assessment-grade'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+import { Checkbox } from './ui/checkbox'
+
+const removeAssessmentGradeFormSchema = z.object({
+  vf: z.boolean(),
+  avi: z.boolean().optional(),
+  avii: z.boolean().optional(),
+  vfe: z.boolean().optional(),
+})
+
+type RemoveAssessmentGradeFormSchema = z.infer<
+  typeof removeAssessmentGradeFormSchema
+>
+
+interface RemoveAssessmentGradeFormProps {
+  studentId: string
+}
+
+export function RemoveAssessmentGradeForm({
+  studentId,
+}: RemoveAssessmentGradeFormProps) {
+  const [searchParams] = useSearchParams()
+  const { disciplineId } = useParams()
+
+  const courseId = searchParams.get('courseId')
+
+  const { mutateAsync: removeAssessmentGradeFn } = useRemoveAssessmentGrade()
+
+  const { handleSubmit, control } = useForm<RemoveAssessmentGradeFormSchema>({
+    resolver: zodResolver(removeAssessmentGradeFormSchema),
+  })
+
+  async function handleRemoveAssessmentGrade({
+    vf,
+    avi,
+    avii,
+    vfe,
+  }: RemoveAssessmentGradeFormSchema) {
+    try {
+      await removeAssessmentGradeFn({
+        courseId: String(courseId),
+        disciplineId: String(disciplineId),
+        studentId,
+        vf: vf ? -1 : undefined,
+        avi: avi ? -1 : undefined,
+        avii: avii ? -1 : undefined,
+        vfe: vfe ? -1 : undefined,
+      })
+      toast.success('Notas adicionadas com sucesso!')
+    } catch (error) {
+      toast.error('Ocorreu algum error ao criar a nota.', {
+        duration: 2000,
+        closeButton: true,
+      })
+    }
+  }
+
+  return (
+    <form
+      className="flex flex-col items-center justify-center gap-4"
+      onSubmit={handleSubmit(handleRemoveAssessmentGrade)}
+    >
+      <div className="flex items-center justify-center gap-8">
+        <div className="flex flex-col items-center">
+          <label htmlFor="vf">VF</label>
+          <Controller
+            name="vf"
+            defaultValue={true}
+            control={control}
+            render={({ field: { name, onChange, value, disabled } }) => {
+              return (
+                <Checkbox
+                  name={name}
+                  checked={value}
+                  disabled={disabled}
+                  onCheckedChange={onChange}
+                  className="h-6 w-6"
+                  id="vf"
+                />
+              )
+            }}
+          ></Controller>
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="avi">AVI</label>
+          <Controller
+            name="avi"
+            defaultValue={false}
+            control={control}
+            render={({ field: { name, onChange, value, disabled } }) => {
+              return (
+                <Checkbox
+                  name={name}
+                  checked={value}
+                  disabled={disabled}
+                  onCheckedChange={onChange}
+                  className="h-6 w-6"
+                  id="vf"
+                />
+              )
+            }}
+          ></Controller>
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="avii">AVII</label>
+          <Controller
+            name="avii"
+            defaultValue={true}
+            control={control}
+            render={({ field: { name, onChange, value, disabled } }) => {
+              return (
+                <Checkbox
+                  name={name}
+                  checked={value}
+                  disabled={disabled}
+                  onCheckedChange={onChange}
+                  className="h-6 w-6"
+                  id="vf"
+                />
+              )
+            }}
+          ></Controller>
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="vfe">VFE</label>
+          <Controller
+            name="vfe"
+            defaultValue={true}
+            control={control}
+            render={({ field: { name, onChange, value, disabled } }) => {
+              return (
+                <Checkbox
+                  name={name}
+                  checked={value}
+                  disabled={disabled}
+                  onCheckedChange={onChange}
+                  className="h-6 w-6"
+                  id="vf"
+                />
+              )
+            }}
+          ></Controller>
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="rounded bg-pmpa-blue-600 px-4 py-2 text-white hover:bg-pmpa-blue-500"
+      >
+        Remover
+      </button>
+    </form>
+  )
+}
---
+++ src/components/update-assessment-form.tsx
@@ -0,0 +1,124 @@
+import { useUpdateAssessment } from '@/hooks/use-update-assessment'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+const updateAssessmentFormSchema = z.object({
+  vf: z.string().optional(),
+  avi: z.string().optional(),
+  avii: z.string().optional(),
+  vfe: z.string().optional(),
+})
+
+type UpdateAssessmentFormSchema = z.infer<typeof updateAssessmentFormSchema>
+
+interface UpdateAssessmentFormProps {
+  studentId: string
+}
+
+export function UpdateAssessmentForm({ studentId }: UpdateAssessmentFormProps) {
+  const [searchParams] = useSearchParams()
+  const { disciplineId } = useParams()
+
+  const courseId = searchParams.get('courseId')
+
+  const { mutateAsync: updateAssessmentFn } = useUpdateAssessment()
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<UpdateAssessmentFormSchema>({
+    resolver: zodResolver(updateAssessmentFormSchema),
+    defaultValues: {},
+  })
+
+  async function handleUpdateAssessment({
+    vf,
+    avi,
+    avii,
+    vfe,
+  }: UpdateAssessmentFormSchema) {
+    try {
+      await updateAssessmentFn({
+        courseId: String(courseId),
+        disciplineId: String(disciplineId),
+        studentId,
+        vf: vf !== undefined ? Number(vf) : undefined,
+        avi: avi !== undefined ? Number(avi) : undefined,
+        avii: avii !== undefined ? Number(avii) : undefined,
+        vfe: vfe !== undefined ? Number(vfe) : undefined,
+      })
+
+      toast.success('Notas adicionadas com sucesso!')
+    } catch (error) {
+      toast.error('Ocorreu algum error ao criar a nota.', {
+        duration: 2000,
+        closeButton: true,
+      })
+    }
+  }
+
+  return (
+    <form className="space-y-4" onSubmit={handleSubmit(handleUpdateAssessment)}>
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col items-center">
+          <label>VF</label>
+          <input
+            type="text"
+            placeholder="0,00"
+            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+            {...register('vf')}
+          />
+          {errors.vf && (
+            <span className="text-sm text-red-500">{errors.vf.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col items-center">
+          <label>AVI</label>
+          <input
+            type="text"
+            placeholder="0,00"
+            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+            {...register('avi')}
+          />
+          {errors.avi && (
+            <span className="text-sm text-red-500">{errors.avi.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col items-center">
+          <label>AVII</label>
+          <input
+            type="text"
+            placeholder="0,00"
+            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+            {...register('avii')}
+          />
+          {errors.avii && (
+            <span className="text-sm text-red-500">{errors.avii.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col items-center">
+          <label>VFE</label>
+          <input
+            type="text"
+            placeholder="0,00"
+            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+            {...register('vfe')}
+          />
+          {errors.vfe && (
+            <span className="text-sm text-red-500">{errors.vfe.message}</span>
+          )}
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="ml-auto block rounded bg-pmpa-blue-600 px-4 py-2 text-white hover:bg-pmpa-blue-500"
+      >
+        Adicionar
+      </button>
+    </form>
+  )
+}
