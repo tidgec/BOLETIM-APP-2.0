6ab833c24a3e7e066e264bb3af7646716d0b75a2
igorabreu29 <abreusense@gmail.com>
2024-08-30 12:31:02 -0300

    feat: behaviors batch

--- src/pages/add-behaviors-batch.tsx
+++ src/pages/add-behaviors-batch.tsx
@@ -1,4 +1,63 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { useCreateBehaviorsBatch } from '@/hooks/use-create-behaviors-batch'
+
+const addBehaviorsBatchSchema = z.object({
+  excel: z
+    .instanceof(FileList)
+    .transform((file) => file.item(0)!)
+    .refine(
+      (file) => file.size <= 5 * 1024 * 1024,
+      'Arquivo até no máximo 5MB',
+    ),
+})
+
+type AddBehaviorsBatchSchema = z.infer<typeof addBehaviorsBatchSchema>
+
 export function AddBehaviorsBatch() {
+  const [searchParams] = useSearchParams()
+  const courseId = searchParams.get('courseId')
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<AddBehaviorsBatchSchema>({
+    resolver: zodResolver(addBehaviorsBatchSchema),
+  })
+
+  const { mutateAsync: createBehaviorsBatchFn } = useCreateBehaviorsBatch()
+
+  async function handleAddBehaviorsBatch({ excel }: AddBehaviorsBatchSchema) {
+    const uploadFormData = new FormData()
+    uploadFormData.set('excel', excel)
+
+    try {
+      await createBehaviorsBatchFn({
+        formData: uploadFormData,
+        courseId: String(courseId),
+      })
+
+      toast.success('Comportamentos adicionados com sucesso!', {
+        duration: 1000,
+      })
+
+      reset()
+    } catch (error) {
+      const err = error as AxiosError
+
+      toast.error(err.response?.data?.message, {
+        duration: 1000,
+      })
+    }
+  }
+
   return (
     <div className="w-full py-6">
       <section className="mx-auto w-full max-w-[90rem]">
@@ -6,7 +65,7 @@ export function AddBehaviorsBatch() {
           Adicionar comportamentos em lote
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit(handleAddBehaviorsBatch)}>
           <div className="mb-4 py-8">
             <label
               htmlFor="file"
@@ -17,21 +76,22 @@ export function AddBehaviorsBatch() {
             <input
               type="file"
               id="file"
-              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-slate-700 shadow focus:outline-none"
+              {...register('excel')}
             />
+
+            {errors.excel && (
+              <span className="text-sm text-red-500">
+                {errors.excel.message}
+              </span>
+            )}
           </div>
           <div className="flex justify-end space-x-4">
             <button
               type="submit"
               className="focus:shadow-outline rounded bg-pmpa-blue-500 px-4 py-2 font-bold text-white hover:bg-pmpa-blue-700 focus:outline-none"
             >
-              Inserir Comportamentos
-            </button>
-            <button
-              className="focus:shadow-outline rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-slate-700 focus:outline-none"
-              onClick={() => window.history.back()}
-            >
-              Voltar
+              Inserir
             </button>
           </div>
         </form>
---
+++ src/pages/behavior-batch.tsx
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom'
+
+import PlusIcon from '@/assets/add-plus-icon.png'
+import RemoveIcon from '@/assets/remove-icon.png'
+import UpdateIcon from '@/assets/update-icon.png'
+
+export function BehaviorBatchPage() {
+  return (
+    <section className="mx-auto w-full max-w-[90rem] px-2 py-10 md:py-6">
+      <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+        Notas de comportamento em lote
+      </h2>
+
+      <div className="mx-auto my-6 grid w-full max-w-7xl grid-cols-1 justify-items-center gap-4 py-2 md:grid-cols-2 md:justify-items-start lg:grid-cols-3">
+        <Link
+          to={'/behaviors/batch/add/courses'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={PlusIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">
+            Adicionar comportamentos em lote
+          </span>
+        </Link>
+        <Link
+          to={'/behaviors/batch/update/courses'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={UpdateIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">
+            Atualizar comportamentos em lote
+          </span>
+        </Link>
+        <Link
+          to={'/behaviors/batch/remove/courses'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={RemoveIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">
+            Remover comportamentos em lote
+          </span>
+        </Link>
+      </div>
+    </section>
+  )
+}
