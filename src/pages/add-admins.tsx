ee54a622581590ecd114ed82ee642deac9438f48
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-30 16:23:25 -0300

    adjust responsive design admin

--- src/pages/add-admins.tsx
+++ src/pages/add-admins.tsx
@@ -88,135 +88,136 @@ export function AddAdmins() {
 
   return (
     <div className="w-full py-6">
-      <section className="mx-auto w-full max-w-[90rem]">
-        <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
-          Adicionar Administrator
-        </h2>
-
-        <div className="group relative my-8 rounded">
-          <form className="space-y-2" onSubmit={handleSubmit(handleAddAdmin)}>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="space-y-2 rounded bg-pmpa-blue-700 p-4">
-                <div className="space-y-1">
-                  <label htmlFor="name" className="text-sm text-gray-200">
-                    Nome completo:
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
-                    placeholder="Digite seu nome completo..."
-                    {...register('username')}
-                  />
-                  {errors.username && (
-                    <span className="text-sm text-red-500">
-                      {errors.username.message}
-                    </span>
-                  )}
-                </div>
-                <div className="space-y-1">
-                  <label htmlFor="cpf" className="text-sm text-gray-200">
-                    CPF:
-                  </label>
-                  <input
-                    type="text"
-                    id="cpf"
-                    className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
-                    placeholder="Digite seu CPF..."
-                    value={cpf}
-                    maxLength={14}
-                    autoComplete="off"
-                    {...register('cpf')}
-                  />
-                  {errors.cpf && (
-                    <span className="text-sm text-red-500">
-                      {errors.cpf.message}
-                    </span>
-                  )}
-                </div>
-                <div className="space-y-1">
-                  <label htmlFor="email" className="text-sm text-gray-200">
-                    E-mail:
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
-                    placeholder="Digite seu email..."
-                    {...register('email')}
-                  />
-                  {errors.email && (
-                    <span className="text-sm text-red-500">
-                      {errors.email.message}
-                    </span>
-                  )}
-                </div>
+    <section className="text-center sm:text-left px-4 mx-auto w-full max-w-[90rem]">
+      <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+        Adicionar Administrador
+      </h2>
+  
+      <div className="group relative my-8 rounded">
+        <form className="space-y-4" onSubmit={handleSubmit(handleAddAdmin)}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div className="space-y-2 rounded bg-pmpa-blue-700 p-4">
+              <div className="space-y-1">
+                <label htmlFor="name" className="text-sm text-gray-200">
+                  Nome completo:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
+                  placeholder="Digite seu nome completo..."
+                  {...register('username')}
+                />
+                {errors.username && (
+                  <span className="text-sm text-red-500">
+                    {errors.username.message}
+                  </span>
+                )}
               </div>
-
-              <div className="space-y-2 rounded bg-pmpa-blue-700 p-4">
-                <div className="space-y-1">
-                  <label htmlFor="password" className="text-sm text-gray-200">
-                    Senha:
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
-                    placeholder="Digite sua senha..."
-                    {...register('password')}
-                  />
-                  {errors.civilId && (
-                    <span className="text-sm text-red-500">
-                      {errors.civilId.message}
-                    </span>
-                  )}
-                </div>
-                <div className="space-y-1">
-                  <label htmlFor="civilId" className="text-sm text-gray-200">
-                    RG Civil:
-                  </label>
-                  <input
-                    type="text"
-                    id="civil"
-                    className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
-                    placeholder="Digite seu RG CIVIL..."
-                    {...register('civilId')}
-                  />
-                  {errors.civilId && (
-                    <span className="text-sm text-red-500">
-                      {errors.civilId.message}
-                    </span>
-                  )}
-                </div>
-                <div className="space-y-1">
-                  <label htmlFor="data" className="text-sm text-gray-200">
-                    Data de nascimento:
-                  </label>
-                  <input
-                    type="date"
-                    id="data"
-                    className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
-                    placeholder="Digite sua data de nascimento..."
-                    {...register('birthday')}
-                  />
-                  {errors.birthday && (
-                    <span className="text-sm text-red-500">
-                      {errors.birthday.message}
-                    </span>
-                  )}
-                </div>
+              <div className="space-y-1">
+                <label htmlFor="cpf" className="text-sm text-gray-200">
+                  CPF:
+                </label>
+                <input
+                  type="text"
+                  id="cpf"
+                  className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
+                  placeholder="Digite seu CPF..."
+                  value={cpf}
+                  maxLength={14}
+                  autoComplete="off"
+                  {...register('cpf')}
+                />
+                {errors.cpf && (
+                  <span className="text-sm text-red-500">
+                    {errors.cpf.message}
+                  </span>
+                )}
+              </div>
+              <div className="space-y-1">
+                <label htmlFor="email" className="text-sm text-gray-200">
+                  E-mail:
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
+                  placeholder="Digite seu email..."
+                  {...register('email')}
+                />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
               </div>
             </div>
-
-            <button
-              type="submit"
-              className="ml-auto block rounded bg-pmpa-blue-500 px-3 py-2 text-white hover:bg-pmpa-blue-700"
-            >
-              Adicionar
-            </button>
-          </form>
-        </div>
-      </section>
-    </div>
+  
+            <div className="space-y-2 rounded bg-pmpa-blue-700 p-4">
+              <div className="space-y-1">
+                <label htmlFor="password" className="text-sm text-gray-200">
+                  Senha:
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
+                  placeholder="Digite sua senha..."
+                  {...register('password')}
+                />
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+              <div className="space-y-1">
+                <label htmlFor="civilId" className="text-sm text-gray-200">
+                  RG Civil:
+                </label>
+                <input
+                  type="text"
+                  id="civilId"
+                  className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
+                  placeholder="Digite seu RG CIVIL..."
+                  {...register('civilId')}
+                />
+                {errors.civilId && (
+                  <span className="text-sm text-red-500">
+                    {errors.civilId.message}
+                  </span>
+                )}
+              </div>
+              <div className="space-y-1">
+                <label htmlFor="birthday" className="text-sm text-gray-200">
+                  Data de nascimento:
+                </label>
+                <input
+                  type="date"
+                  id="birthday"
+                  className="w-full rounded-sm px-4 py-3 text-sm text-gray-700"
+                  placeholder="Digite sua data de nascimento..."
+                  {...register('birthday')}
+                />
+                {errors.birthday && (
+                  <span className="text-sm text-red-500">
+                    {errors.birthday.message}
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
+  
+          <button
+            type="submit"
+            className="ml-auto block rounded bg-pmpa-blue-500 px-3 py-2 text-white hover:bg-pmpa-blue-700"
+          >
+            Adicionar
+          </button>
+        </form>
+      </div>
+    </section>
+  </div>
+  
   )
 }
