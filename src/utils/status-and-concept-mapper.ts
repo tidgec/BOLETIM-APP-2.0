79d7233d70ff2967815f696548ffdb6a7eb71a2b
igorabreu29 <abreusense@gmail.com>
2024-09-02 14:10:28 -0300

    feat: add status and concept mapper

---
+++ src/utils/status-and-concept-mapper.ts
@@ -0,0 +1,50 @@
+type BehaviorStatus = 'disapproved' | 'approved'
+
+type Concept =
+  | 'excellent'
+  | 'very good'
+  | 'good'
+  | 'regular'
+  | 'insufficient'
+  | 'no income'
+
+type GeralStatus =
+  | 'approved'
+  | 'disapproved'
+  | 'approved second season'
+  | 'disapproved second season'
+  | 'second season'
+
+type Status =
+  | 'approved'
+  | 'disapproved'
+  | 'approved second season'
+  | 'second season'
+export const behaviorStatusMap: Record<BehaviorStatus, string> = {
+  approved: 'Aprovado',
+  disapproved: 'Reprovado',
+}
+
+export const conceptMap: Record<Concept, string> = {
+  excellent: 'Excelente',
+  'very good': 'Muito bom',
+  good: 'Bom',
+  regular: 'Regular',
+  insufficient: 'Insuficiente',
+  'no income': 'Sem renda',
+}
+
+export const overallStatusMap: Record<GeralStatus, string> = {
+  approved: 'Aprovado',
+  disapproved: 'Reprovado',
+  'approved second season': 'Aprovado em segunda época',
+  'disapproved second season': 'Reprovado em segunda época',
+  'second season': 'Segunda época',
+}
+
+export const statusMap: Record<Status, string> = {
+  approved: 'Aprovado',
+  disapproved: 'Reprovado',
+  'approved second season': 'Aprovado em segunda época',
+  'second season': 'Segunda época',
+}
