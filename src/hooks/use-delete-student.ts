6abb85d6622027f18d0dcf65412fdc70dae315f9
igorabreu29 <abreusense@gmail.com>
2024-09-10 19:14:49 -0300

    feat: delete student logic

--- src/hooks/use-delete-student.ts
+++ src/hooks/use-delete-student.ts
@@ -1,10 +1,36 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { deleteStudent } from '@/http/delete-student'
+import type { GetCourseStudentsResponse } from '@/http/get-course-students'
 
 export function useDeleteStudent() {
+  const queryClient = useQueryClient()
+
   const mutation = useMutation({
     mutationFn: deleteStudent,
+    onSuccess: (_, { id }) => {
+      const cached = queryClient.getQueryData<GetCourseStudentsResponse>([
+        'student-courses',
+      ])
+
+      console.log(cached)
+
+      if (cached) {
+        queryClient.setQueryData<GetCourseStudentsResponse>(
+          ['student-courses'],
+          {
+            ...cached,
+            students: cached.students.filter((student) => {
+              return student.id === id
+            }),
+            totalItems: cached.totalItems - 1,
+            pages: cached.pages - 1,
+          },
+        )
+      }
+
+      return cached
+    },
   })
 
   return mutation
