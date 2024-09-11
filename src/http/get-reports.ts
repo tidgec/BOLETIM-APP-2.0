c5fc008ba97ec0e33b26c23cd8d549bb263fd5d2
igorabreu29 <abreusense@gmail.com>
2024-09-11 12:30:55 -0300

    feat: add pagination in report

--- src/http/get-reports.ts
+++ src/http/get-reports.ts
@@ -2,17 +2,12 @@ import Cookies from 'js-cookie'
 
 import { api } from '@/lib/axios'
 
-interface GetReportsResponse {
-  id: string
-  title: string
-  content: string
-  ip: string
-  createdAt: string
-  fileLink?: string
-  fileName?: string
+export interface GetReportsRequest {
+  page?: string
+  action?: string
 }
 
-interface GetReportsAxiosResponse {
+interface GetReportsResponse {
   reports: {
     id: string
     title: string
@@ -22,16 +17,30 @@ interface GetReportsAxiosResponse {
     fileLink?: string
     fileName?: string
   }[]
+
+  totalItems: number
+  pages: number
 }
 
-export async function getReports(): Promise<GetReportsResponse[]> {
+export async function getReports({
+  action,
+  page,
+}: GetReportsRequest): Promise<GetReportsResponse> {
   const token = Cookies.get('token')
 
-  const response = await api.get<GetReportsAxiosResponse>('/reports', {
+  const response = await api.get<GetReportsResponse>('/reports', {
     headers: {
       Authorization: `Bearer ${token}`,
     },
+    params: {
+      action: action === 'all' ? undefined : action,
+      page,
+    },
   })
 
-  return response.data.reports
+  return {
+    reports: response.data.reports,
+    pages: response.data.pages,
+    totalItems: response.data.totalItems,
+  }
 }
