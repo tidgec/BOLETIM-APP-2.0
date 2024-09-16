6efbd38cb50ae2eb68de9768e293b92c04f7e26f
Edgar-Klewert <edgarklewert66@gmail.com>
2024-09-16 17:05:29 -0300

    feat: design adjustment for skeleton overall ranking animation

--- src/pages/overall-ranking.tsx
+++ src/pages/overall-ranking.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from 'react-router-dom'
 
 import { Chart } from '@/components/chart'
 import GeneralClassificationViewer from '@/components/templates/general-classification-viewer'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useGetRanking } from '@/hooks/use-get-ranking'
 import { conceptMap, overallStatusMap } from '@/utils/status-and-concept-mapper'
 
@@ -64,42 +65,6 @@ export function OverallRanking() {
       polo: 'SANTARÉM',
       status: 'APROVADO',
     },
-    {
-      class: '2º',
-      qav: '30/30',
-      qc: '8/10',
-      rg: '23751',
-      name: 'Mariana Souza Ferreira',
-      average: '9.724',
-      concept: 'Muito Bom',
-      dob: '01/01/1924',
-      polo: 'SANTARÉM',
-      status: 'APROVADO',
-    },
-    {
-      class: '3º',
-      qav: '27/30',
-      qc: '9/10',
-      rg: '23751',
-      name: 'Eduardo Alves Lima',
-      average: '9.700',
-      concept: 'Muito Bom',
-      dob: '01/01/2000',
-      polo: 'SANTARÉM',
-      status: 'APROVADO',
-    },
-    {
-      class: '4º',
-      qav: '20/30',
-      qc: '5/10',
-      rg: '23751',
-      name: 'Camila Rocha Costa',
-      average: '9.724',
-      concept: 'Muito Bom',
-      dob: '01/01/1924',
-      polo: 'SANTARÉM',
-      status: 'APROVADO',
-    },
   ]
 
   return (
@@ -108,43 +73,47 @@ export function OverallRanking() {
         <h2 className="mb-4 border-b-2 border-b-black text-xl font-semibold">
           Classificação
         </h2>
-        
+
         <div className="mb-4 flex items-center justify-center rounded-lg bg-pmpa-blue-500 p-4">
           <div>
-            <Chart
-              charts={[
-                {
-                  status: 'excellent',
-                  size: totalExcellentSize ?? 0,
-                  fill: 'var(--color-excellent)',
-                },
-                {
-                  status: 'very good',
-                  size: totalVeryGoodSize ?? 0,
-                  fill: 'var(--color-very-good)',
-                },
-                {
-                  status: 'good',
-                  size: totalGoodSize ?? 0,
-                  fill: 'var(--color-good)',
-                },
-                {
-                  status: 'regular',
-                  size: totalRegularSize ?? 0,
-                  fill: 'var(--color-regular)',
-                },
-                {
-                  status: 'insufficient',
-                  size: totalInsufficientSize ?? 0,
-                  fill: 'var(--color-insufficient)',
-                },
-                {
-                  status: 'no income',
-                  size: totalNoIncomeSize ?? 0,
-                  fill: 'var(--color-no-income)',
-                },
-              ]}
-            />
+            {isLoading ? (
+              <Skeleton className="h-80 w-80" />
+            ) : (
+              <Chart
+                charts={[
+                  {
+                    status: 'excellent',
+                    size: totalExcellentSize ?? 0,
+                    fill: 'var(--color-excellent)',
+                  },
+                  {
+                    status: 'very good',
+                    size: totalVeryGoodSize ?? 0,
+                    fill: 'var(--color-very-good)',
+                  },
+                  {
+                    status: 'good',
+                    size: totalGoodSize ?? 0,
+                    fill: 'var(--color-good)',
+                  },
+                  {
+                    status: 'regular',
+                    size: totalRegularSize ?? 0,
+                    fill: 'var(--color-regular)',
+                  },
+                  {
+                    status: 'insufficient',
+                    size: totalInsufficientSize ?? 0,
+                    fill: 'var(--color-insufficient)',
+                  },
+                  {
+                    status: 'no income',
+                    size: totalNoIncomeSize ?? 0,
+                    fill: 'var(--color-no-income)',
+                  },
+                ]}
+              />
+            )}
           </div>
         </div>
 
@@ -153,92 +122,127 @@ export function OverallRanking() {
         </div>
 
         <div className="overflow-x-auto rounded-lg border border-gray-200 bg-white shadow-md">
-          <table className="min-w-full">
-            <thead>
-              <tr className="border-b bg-pmpa-blue-500">
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  CLASS
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  Q.AV
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  Q.C
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  RG
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  NOME COMPLETO
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  MÉDIA FINAL
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  CONCEITO
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  DATA DE NASCIMENTO
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  POLO
-                </th>
-                <th className="px-4 py-2 text-left text-sm font-semibold text-white">
-                  STATUS
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {isLoading ? (
-                <p>Loading...</p>
-              ) : (
-                ranking?.map((item, index) => (
-                  <tr key={index}>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {index + 1}º
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentAverage.assessmentsCount}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentAverage.averageInform.behaviorsCount}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentCivilID}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentPole}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentAverage.averageInform.geralAverage}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {
-                        conceptMap[
-                          item.studentAverage.averageInform.studentAverageStatus
-                            .concept
-                        ]
-                      }
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentBirthday}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {item.studentPole}
-                    </td>
-                    <td className="px-4 py-2 text-sm text-slate-700">
-                      {
-                        overallStatusMap[
-                          item.studentAverage.averageInform.studentAverageStatus
-                            .status
-                        ]
-                      }
-                    </td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+          {isLoading ? (
+            <Skeleton className="h-72 w-full" />
+          ) : (
+            <table className="min-w-full">
+              <thead>
+                <tr className="border-b bg-pmpa-blue-500">
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    CLASS
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    Q.AV
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    Q.C
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    RG
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    NOME COMPLETO
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    MÉDIA FINAL
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    CONCEITO
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    DATA DE NASCIMENTO
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    POLO
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold text-white">
+                    STATUS
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {isLoading
+                  ? Array.from({ length: 10 }).map((_, index) => (
+                      <tr key={index}>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-12" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-20" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-20" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-24" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-32" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-24" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-24" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-32" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-32" />
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          <Skeleton className="h-6 w-24" />
+                        </td>
+                      </tr>
+                    ))
+                  : ranking?.map((item, index) => (
+                      <tr key={index}>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {index + 1}º
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentAverage.assessmentsCount}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentAverage.averageInform.behaviorsCount}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentCivilID}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentPole}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentAverage.averageInform.geralAverage}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {
+                            conceptMap[
+                              item.studentAverage.averageInform
+                                .studentAverageStatus.concept
+                            ]
+                          }
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentBirthday}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {item.studentPole}
+                        </td>
+                        <td className="px-4 py-2 text-sm text-slate-700">
+                          {
+                            overallStatusMap[
+                              item.studentAverage.averageInform
+                                .studentAverageStatus.status
+                            ]
+                          }
+                        </td>
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
+          )}
         </div>
 
         <div className="mt-4 text-center">
