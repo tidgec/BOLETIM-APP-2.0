a1902d8a0bad06afd86e89925cb01d7271ba20e5
Edgar-Klewert <edgarklewert66@gmail.com>
2024-09-24 17:55:04 -0300

    feat: animation skeleton loading add list behavior

--- src/pages/list-behavior.tsx
+++ src/pages/list-behavior.tsx
@@ -1,4 +1,5 @@
 import { Course } from '@/components/course'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useGetCourses } from '@/hooks/use-get-courses'
 
 export function ListBehaviorPage() {
@@ -12,16 +13,21 @@ export function ListBehaviorPage() {
         </h2>
 
         <div className="flex flex-wrap justify-center">
-          {isLoading && <p>Loading...</p>}
-          {!isLoading &&
-            courses?.map((course) => (
-              <div
-                key={course.id}
-                className="m-10 w-80 bg-white py-1 shadow-md"
-              >
-                <Course course={course} />
-              </div>
-            ))}
+          {isLoading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <div key={index} className="m-10 w-80 bg-white py-1 shadow-md">
+                  <Skeleton className="mb-2 h-40 w-full" />
+                  <Skeleton className="mx-auto h-6 w-3/4" />
+                </div>
+              ))
+            : courses?.map((course) => (
+                <div
+                  key={course.id}
+                  className="m-10 w-80 bg-white py-1 shadow-md"
+                >
+                  <Course course={course} />
+                </div>
+              ))}
         </div>
       </section>
     </div>
