7f67efb2f4e53729f24acaca77b38c198be476f2
Edgar-Klewert <edgarklewert66@gmail.com>
2024-09-24 17:54:02 -0300

    feat: skeleton add loading delete courses

--- src/pages/delete-courses.tsx
+++ src/pages/delete-courses.tsx
@@ -16,6 +16,7 @@ import {
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useDeleteCourse } from '@/hooks/use-delete-course'
 import { useGetCourses } from '@/hooks/use-get-courses'
 
@@ -26,13 +27,11 @@ export function DeleteCourses() {
 
   const { courses, totalItems, pages, isLoading } = useGetCourses(page ?? '1')
 
-  const { mutateAsync: deleteCouseFn } = useDeleteCourse()
+  const { mutateAsync: deleteCourseFn } = useDeleteCourse()
 
   async function handleDeleteCourse(id: string) {
     try {
-      await deleteCouseFn({
-        id,
-      })
+      await deleteCourseFn({ id })
 
       toast.success('Curso deletado com sucesso!', {
         duration: 1000,
@@ -54,42 +53,46 @@ export function DeleteCourses() {
         </h2>
 
         <div className="mx-2 my-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
-          {isLoading && <p>Loading...</p>}
-          {!isLoading &&
-            courses?.map((course) => (
-              <AlertDialog key={course.id}>
-                <AlertDialogTrigger>
-                  <div
-                    key={course.id}
-                    className="bg-white text-center shadow-md"
-                  >
-                    <Course course={course} />
-                  </div>
-                </AlertDialogTrigger>
+          {isLoading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <div key={index} className="bg-white text-center shadow-md">
+                  <Skeleton className="mb-2 h-32 w-full" />
+                  <Skeleton className="mx-auto h-6 w-3/4" />
+                </div>
+              ))
+            : courses?.map((course) => (
+                <AlertDialog key={course.id}>
+                  <AlertDialogTrigger>
+                    <div className="bg-white text-center shadow-md">
+                      <Course course={course} />
+                    </div>
+                  </AlertDialogTrigger>
 
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      Deseja remover esse curso?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Esta ação não pode ser desfeita. Caso clique em continuar,
-                      todos os registros desse curso serão deletados.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Deseja remover esse curso?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        Esta ação não pode ser desfeita. Caso clique em
+                        continuar, todos os registros desse curso serão
+                        deletados.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
 
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Não</AlertDialogCancel>
-                    <AlertDialogAction asChild>
-                      <Button onClick={() => handleDeleteCourse(course.id)}>
-                        Sim
-                      </Button>
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            ))}
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Não</AlertDialogCancel>
+                      <AlertDialogAction asChild>
+                        <Button onClick={() => handleDeleteCourse(course.id)}>
+                          Sim
+                        </Button>
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              ))}
         </div>
+
         <Pagination
           items={totalItems ?? 0}
           page={page ? Number(page) : 1}
