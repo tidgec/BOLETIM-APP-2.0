46e42e937a5593944b949983814717360d39ba53
igorabreu29 <abreusense@gmail.com>
2024-09-25 22:06:47 -0300

    feat: add logic to manager

--- src/http/create-ranking-sheet.ts
+++ src/http/create-ranking-sheet.ts
@@ -1,6 +1,8 @@
 import Cookies from 'js-cookie'
+import { jwtDecode } from 'jwt-decode'
 
 import { api } from '@/lib/axios'
+import type { JWTPayload } from '@/types/jwt'
 
 interface CreateRankingSheetRequest {
   courseId: string
@@ -16,6 +18,29 @@ export async function createRankingSheet({
   hasBehavior,
 }: CreateRankingSheetRequest) {
   const token = Cookies.get('token')
+  if (!token) throw new Error('Não autorizado.')
+
+  const { payload }: JWTPayload = jwtDecode(token)
+
+  if (payload.role === 'manager') {
+    const response = await api.post<CreateRankingSheetResponse>(
+      `/courses/${courseId}/manager/classification/sheet`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+
+        params: {
+          hasBehavior: hasBehavior ?? 'true',
+        },
+      },
+    )
+
+    return {
+      fileUrl: response.data.fileUrl,
+    }
+  }
 
   const response = await api.post<CreateRankingSheetResponse>(
     `/courses/${courseId}/classification/sheet`,
@@ -26,7 +51,7 @@ export async function createRankingSheet({
       },
 
       params: {
-        hasBehavior,
+        hasBehavior: hasBehavior ?? 'true',
       },
     },
   )
