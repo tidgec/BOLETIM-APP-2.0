c7fc262131f0b532b025aa933aded05116760d1b
igorabreu29 <abreusense@gmail.com>
2024-10-04 14:14:59 -0300

    feat: cache

--- src/hooks/use-update-admin.ts
+++ src/hooks/use-update-admin.ts
@@ -1,10 +1,48 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
+import { SearchResponse } from '@/http/search'
 import { updateAdmin } from '@/http/update-admin'
 
 export function useUpdateAdmin() {
+  const queryClient = useQueryClient()
+
   const mutation = useMutation({
     mutationFn: updateAdmin,
+    onSuccess: async (_, { cpf, username, id }) => {
+      const serchsCache = queryClient.getQueriesData<SearchResponse>({
+        queryKey: ['search'],
+      })
+
+      serchsCache.forEach(([cacheKey, cached]) => {
+        if (!cached) return
+
+        let pages: number = 1
+
+        if (cached.totalItems > 10) {
+          pages = cached.pages
+        }
+
+        pages =
+          cached.totalItems <= 10 && cached.pages === 1
+            ? cached.pages
+            : cached.pages - 1
+
+        queryClient.setQueryData<SearchResponse>(cacheKey, {
+          ...cached,
+          searchs: cached.searchs.map((search) => {
+            const isUser = search.id === id
+
+            return {
+              ...search,
+              username: isUser && username ? username : search.username,
+              cpf: isUser && cpf ? cpf : search.cpf,
+            }
+          }),
+          totalItems: cached.totalItems - 1,
+          pages,
+        })
+      })
+    },
   })
 
   return mutation
--- src/hooks/use-update-student.ts
+++ src/hooks/use-update-student.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
+import { GetCourseStudentsResponse } from '@/http/get-course-students'
 import { SearchResponse } from '@/http/search'
 import { updateStudent } from '@/http/update-student'
 
@@ -8,9 +9,45 @@ export function useUpdateStudent() {
 
   const mutation = useMutation({
     mutationFn: updateStudent,
-    onSuccess: async (_, { cpf, username, id }) => {
+    onSuccess: async (_, { cpf, username, email, id }) => {
       const serchsCache = queryClient.getQueriesData<SearchResponse>({
         queryKey: ['search'],
+      })
+
+      const studentCoursesCache =
+        queryClient.getQueriesData<GetCourseStudentsResponse>({
+          queryKey: ['student-courses'],
+        })
+
+      studentCoursesCache.forEach(([cacheKey, cached]) => {
+        if (!cached) return
+
+        let pages: number = 1
+
+        if (cached.totalItems > 10) {
+          pages = cached.pages
+        }
+
+        pages =
+          cached.totalItems <= 10 && cached.pages === 1
+            ? cached.pages
+            : cached.pages - 1
+
+        queryClient.setQueryData<GetCourseStudentsResponse>(cacheKey, {
+          ...cached,
+          students: cached.students.map((student) => {
+            const isUser = student.id === id
+
+            return {
+              ...student,
+              username: isUser && username ? username : student.username,
+              cpf: isUser && cpf ? cpf : student.cpf,
+              email: isUser && email ? email : student.email,
+            }
+          }),
+          totalItems: cached.totalItems - 1,
+          pages,
+        })
       })
 
       serchsCache.forEach(([cacheKey, cached]) => {
