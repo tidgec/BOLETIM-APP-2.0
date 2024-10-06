d8100a08816ade5d92211f066c7dac925920836c
igorabreu29 <abreusense@gmail.com>
2024-10-06 13:36:59 -0300

    feat: get course assessments

--- src/http/get-course-assessments.ts
+++ src/http/get-course-assessments.ts
@@ -4,21 +4,30 @@ import { api } from '@/lib/axios'
 
 export interface GetCourseAssessmentsRequest {
   courseId: string
+  disciplineId: string
 }
 
 export interface GetCourseAssessmentsResponse {
   assessments: {
     id: string
+    courseId: string
+    disciplineId: string
+    studentId: string
+    vf?: number
+    avi?: number
+    avii?: number
+    vfe?: number
   }[]
 }
 
 export async function getCourseAssessments({
   courseId,
-}: GetCourseAssessmentsRequest): Promise<GetCourseAssessmentsResponse> {
+  disciplineId,
+}: GetCourseAssessmentsRequest) {
   const token = Cookies.get('token')
 
   const response = await api.get<GetCourseAssessmentsResponse>(
-    `/courses/${courseId}/assessments`,
+    `/courses/${courseId}/disciplines/${disciplineId}/assessments`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
