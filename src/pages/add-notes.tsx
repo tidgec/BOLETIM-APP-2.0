9f71390346e63d897621454dfc73b18e6bc06172
igorabreu29 <abreusense@gmail.com>
2024-10-07 09:23:24 -0300

    feat: add disciplineId

--- src/pages/add-notes.tsx
+++ src/pages/add-notes.tsx
@@ -1,12 +1,14 @@
-import { useSearchParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 
 import { CreateAssessmentForm } from '@/components/create-assessment-form'
 import { FilterForm } from '@/components/filter/filter-form'
 import { Pagination } from '@/components/pagination'
 import { Skeleton } from '@/components/ui/skeleton'
+import { useGetCourseAssessments } from '@/hooks/use-get-course-assessments'
 import { useGetCourseStudents } from '@/hooks/use-get-course-students'
 
 export function AddNotes() {
+  const { disciplineId } = useParams()
   const [searchParams] = useSearchParams()
 
   const courseId = searchParams.get('courseId')
@@ -23,6 +25,11 @@ export function AddNotes() {
     poleId: poleId ?? 'all',
   })
 
+  const { assessments } = useGetCourseAssessments({
+    courseId: courseId ?? '',
+    disciplineId: disciplineId ?? '',
+  })
+
   return (
     <div className="w-full py-6">
       <section className="mx-auto w-full max-w-[90rem]">
@@ -34,30 +41,46 @@ export function AddNotes() {
 
         {isLoading && (
           <div className="space-y-4">
-            {[...Array(3)].map((_, index) => (
-              <div key={index} className="rounded border p-4">
-                <Skeleton className="mb-2 h-6 w-3/4" />
-                <Skeleton className="mb-1 h-4 w-1/2" />
-                <Skeleton className="h-4 w-full" />
-              </div>
-            ))}
+            <Skeleton className="h-72 w-full bg-gray-300" />
+            <Skeleton className="h-72 w-full bg-gray-300" />
+            <Skeleton className="h-72 w-full bg-gray-300" />
           </div>
         )}
 
-        <div className="mx-2 mb-4 h-[36rem] space-y-4 overflow-auto">
-          {!isLoading &&
-            students?.map((student) => (
-              <div key={student.id} className="rounded border p-4">
-                <h2 className="mb-4 text-lg font-bold">
-                  Nome: {student.username}
-                </h2>
-                <p>Curso: {student.course.name}</p>
-                <p>Polo: {student.pole.name}</p>
+        {!isLoading && (
+          <div className="mx-2 mb-4 h-[36rem] space-y-4 overflow-auto">
+            {students?.map((student) => {
+              if (!assessments) {
+                return (
+                  <div className="space-y-4" key={student.id}>
+                    <Skeleton className="h-72 w-full bg-gray-300" />
+                    <Skeleton className="h-72 w-full bg-gray-300" />
+                    <Skeleton className="h-72 w-full bg-gray-300" />
+                  </div>
+                )
+              }
 
-                <CreateAssessmentForm studentId={student.id} />
-              </div>
-            ))}
-        </div>
+              const studentAssessment = assessments.find(
+                (item) => item.studentId === student.id,
+              )
+
+              return (
+                <div key={student.id} className="rounded border p-4">
+                  <h2 className="mb-4 text-lg font-bold">
+                    Nome: {student.username}
+                  </h2>
+                  <p>Curso: {student.course.name}</p>
+                  <p>Polo: {student.pole.name}</p>
+
+                  <CreateAssessmentForm
+                    studentId={student.id}
+                    assessment={studentAssessment}
+                  />
+                </div>
+              )
+            })}
+          </div>
+        )}
 
         <Pagination
           items={totalItems ?? 0}
