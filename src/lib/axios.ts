5dee1a5191bf517da7ee435e1b573499ad20c42d
igorabreu29 <abreusense@gmail.com>
2024-10-07 10:33:46 -0300

    refactor: change url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+import { env } from '@/env'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
 
