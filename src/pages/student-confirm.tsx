a9e5fdb2405c557e91f5bf374ee35a7054923c40
igorabreu29 <abreusense@gmail.com>
2024-10-07 10:20:57 -0300

    feat: add more user data

--- src/pages/student-confirm.tsx
+++ src/pages/student-confirm.tsx
@@ -6,8 +6,14 @@ import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { useConfirmStudentLogin } from '@/hooks/use-confirm-student-login'
+import { useProfile } from '@/hooks/use-profile'
+import { formatCPF } from '@/utils/format-cpf'
 
 const confirmStudentLoginSchema = z.object({
+  username: z.string(),
+  cpf: z.string(),
+  email: z.string(),
+  civilId: z.string(),
   fatherName: z.string().optional(),
   motherName: z.string(),
   militaryId: z.string().optional(),
@@ -20,8 +26,18 @@ type ConfirmStudentLoginSchema = z.infer<typeof confirmStudentLoginSchema>
 export function StudentConfirm() {
   const navigate = useNavigate()
 
+  const { user } = useProfile()
+
   const { handleSubmit, register, reset } = useForm<ConfirmStudentLoginSchema>({
     resolver: zodResolver(confirmStudentLoginSchema),
+    values: {
+      username: user?.username ?? '',
+      email: user?.email ?? '',
+      civilId: user?.civilId ?? '',
+      cpf: user?.cpf ? formatCPF(user.cpf) : '',
+      militaryId: user?.militaryId ?? '',
+      motherName: user?.motherName ?? '',
+    },
   })
 
   const { mutateAsync: confirmStudentLoginFn } = useConfirmStudentLogin()
@@ -58,7 +74,7 @@ export function StudentConfirm() {
   }
 
   return (
-    <div className="my-20 sm:my-16 lg:my-28">
+    <div className="my-20 sm:my-16">
       <section className="mx-auto w-full max-w-sm space-y-4 rounded bg-pmpa-blue-700 py-8 sm:max-w-md md:max-w-lg lg:max-w-3xl">
         <h2 className="ml-4 text-xl font-medium text-white">
           Confirmação de login
@@ -68,6 +84,32 @@ export function StudentConfirm() {
           className="w-full space-y-4 text-white"
           onSubmit={handleSubmit(handleConfirmStudentLogin)}
         >
+          <div className="flex flex-col space-y-1 px-4 sm:px-6">
+            <label className="text-xs sm:text-sm" htmlFor="name">
+              Nome:
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="rounded px-4 py-2 text-black placeholder:text-xs sm:placeholder:text-sm"
+              placeholder="Digite o seu nome"
+              {...register('username')}
+            />
+          </div>
+
+          <div className="flex flex-col space-y-1 px-4 sm:px-6">
+            <label className="text-xs sm:text-sm" htmlFor="email">
+              E-mail:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="rounded px-4 py-2 text-black placeholder:text-xs sm:placeholder:text-sm"
+              placeholder="Digite o seu nome"
+              {...register('email')}
+            />
+          </div>
+
           <div className="flex flex-col space-y-1 px-4 sm:px-6">
             <label className="text-xs sm:text-sm" htmlFor="fatherName">
               Nome do pai:
@@ -91,6 +133,19 @@ export function StudentConfirm() {
               className="rounded px-4 py-2 text-black placeholder:text-xs sm:placeholder:text-sm"
               placeholder="Digite o nome da sua mãe"
               {...register('motherName')}
+            />
+          </div>
+
+          <div className="flex flex-col space-y-1 px-4 sm:px-6">
+            <label className="text-xs sm:text-sm" htmlFor="civilId">
+              RG Civil:
+            </label>
+            <input
+              type="text"
+              id="civilId"
+              className="rounded px-4 py-2 text-black placeholder:text-xs sm:placeholder:text-sm"
+              placeholder="Digite o nome da sua mãe"
+              {...register('civilId')}
             />
           </div>
 
@@ -137,6 +192,11 @@ export function StudentConfirm() {
             <Button variant={'secondary'}>Confirmar</Button>
           </div>
         </form>
+
+        <div className="px-8 text-white">
+          <p>CURSO: {user?.courses && user.courses[0].name}</p>
+          <p>POLO: {user?.poles && user.poles[0].name}</p>
+        </div>
       </section>
     </div>
   )
