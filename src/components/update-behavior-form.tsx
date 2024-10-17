308dca9c02bec89e4ef8cac6c7afc623bb1f26ef
igorabreu29 <abreusense@gmail.com>
2024-10-17 08:52:35 -0300

    feat: add module and currentYear

--- src/components/update-behavior-form.tsx
+++ src/components/update-behavior-form.tsx
@@ -40,6 +40,8 @@ interface UpdateBehaviorFormProps {
     october?: number | null
     november?: number | null
     december?: number | null
+    currentYear: number
+    module: number
   }
 }
 
@@ -119,184 +121,196 @@ export function UpdateBehaviorForm({ behavior }: UpdateBehaviorFormProps) {
   }
 
   return (
-    <form className="space-y-4" onSubmit={handleSubmit(handleUpdateBehavior)}>
-      <div className="grid grid-cols-3 gap-2">
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Janeiro</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('january')}
-          />
-          {errors.january && (
-            <span className="text-sm text-red-500">
-              {errors.january.message}
-            </span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Fevereiro</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('february')}
-          />
-          {errors.february && (
-            <span className="text-sm text-red-500">
-              {errors.february.message}
-            </span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Março</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('march')}
-          />
-          {errors.march && (
-            <span className="text-sm text-red-500">{errors.march.message}</span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Abril</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('april')}
-          />
-          {errors.april && (
-            <span className="text-sm text-red-500">{errors.april.message}</span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Maio</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('may')}
-          />
-          {errors.may && (
-            <span className="text-sm text-red-500">{errors.may.message}</span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Junho</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('jun')}
-          />
-          {errors.jun && (
-            <span className="text-sm text-red-500">{errors.jun.message}</span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Julho</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('july')}
-          />
-          {errors.july && (
-            <span className="text-sm text-red-500">{errors.july.message}</span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Agosto</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('august')}
-          />
-          {errors.august && (
-            <span className="text-sm text-red-500">
-              {errors.august.message}
-            </span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Setembro</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('september')}
-          />
-          {errors.september && (
-            <span className="text-sm text-red-500">
-              {errors.september.message}
-            </span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Outubro</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('october')}
-          />
-          {errors.october && (
-            <span className="text-sm text-red-500">
-              {errors.october.message}
-            </span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Novembro</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('november')}
-          />
-          {errors.november && (
-            <span className="text-sm text-red-500">
-              {errors.november.message}
-            </span>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <label className="text-sm md:text-base">Dezembro</label>
-          <input
-            type="text"
-            placeholder="0,00"
-            className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
-            {...register('december')}
-          />
-          {errors.december && (
-            <span className="text-sm text-red-500">
-              {errors.december.message}
-            </span>
-          )}
-        </div>
+    <>
+      <div className="space-x-2">
+        <span>Ano: {behavior.currentYear}</span>
+        <span>Módulo: {behavior.module}</span>
       </div>
-      <button
-        type="submit"
-        className="ml-auto block rounded bg-pmpa-blue-600 px-4 py-2 text-white hover:bg-pmpa-blue-500"
-      >
-        Adicionar
-      </button>
-    </form>
+      <form className="space-y-4" onSubmit={handleSubmit(handleUpdateBehavior)}>
+        <div className="grid grid-cols-3 gap-2">
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Janeiro</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('january')}
+            />
+            {errors.january && (
+              <span className="text-sm text-red-500">
+                {errors.january.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Fevereiro</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('february')}
+            />
+            {errors.february && (
+              <span className="text-sm text-red-500">
+                {errors.february.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Março</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('march')}
+            />
+            {errors.march && (
+              <span className="text-sm text-red-500">
+                {errors.march.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Abril</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('april')}
+            />
+            {errors.april && (
+              <span className="text-sm text-red-500">
+                {errors.april.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Maio</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('may')}
+            />
+            {errors.may && (
+              <span className="text-sm text-red-500">{errors.may.message}</span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Junho</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('jun')}
+            />
+            {errors.jun && (
+              <span className="text-sm text-red-500">{errors.jun.message}</span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Julho</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('july')}
+            />
+            {errors.july && (
+              <span className="text-sm text-red-500">
+                {errors.july.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Agosto</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('august')}
+            />
+            {errors.august && (
+              <span className="text-sm text-red-500">
+                {errors.august.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Setembro</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('september')}
+            />
+            {errors.september && (
+              <span className="text-sm text-red-500">
+                {errors.september.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Outubro</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('october')}
+            />
+            {errors.october && (
+              <span className="text-sm text-red-500">
+                {errors.october.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Novembro</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('november')}
+            />
+            {errors.november && (
+              <span className="text-sm text-red-500">
+                {errors.november.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col items-center">
+            <label className="text-sm md:text-base">Dezembro</label>
+            <input
+              type="text"
+              placeholder="0,00"
+              className="w-full rounded border bg-pmpa-blue-500 p-2 text-center text-white"
+              {...register('december')}
+            />
+            {errors.december && (
+              <span className="text-sm text-red-500">
+                {errors.december.message}
+              </span>
+            )}
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="ml-auto block rounded bg-pmpa-blue-600 px-4 py-2 text-white hover:bg-pmpa-blue-500"
+        >
+          Atualizar
+        </button>
+      </form>
+    </>
   )
 }
