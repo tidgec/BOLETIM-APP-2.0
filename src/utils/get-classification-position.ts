748f1bdde5492b029c6d63f1766ccd173c0f71a0
igorabreu29 <abreusense@gmail.com>
2024-10-22 16:44:54 -0300

    refactor: change perPage

--- src/utils/get-classification-position.ts
+++ src/utils/get-classification-position.ts
@@ -1,5 +1,5 @@
 export function getClassificationPosition(index: number, page: string) {
   const pageIndex = page ? Number(page) - 1 : 0
-  const perPage = 30
+  const perPage = 1000
   return pageIndex * perPage + (index + 1)
 }
